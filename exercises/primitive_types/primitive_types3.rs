fcddc7078f5059b5d6b2bb5b9fcfb1518552e12a
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2023-08-19 16:52:31 +0200

    feat: primitive types done

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // Array with 300 elements, filled with value 5
+    let a: [i32; 300] = [5; 300];
+    // Could also be like this let a = [5; 300];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
